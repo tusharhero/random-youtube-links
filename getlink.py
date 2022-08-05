f3ac33619d10b0ed95bb644db62d233c90d8b533
tusharhero <tusharmaharana44676@gmail.com>
2022-08-05 19:07:33 +0530

    changed the json a little

--- getlink.py
+++ getlink.py
@@ -10,11 +10,14 @@ import scrapetube
 import json
 from datetime import date
 
+
 def get_random_link(search_query: str, limit: int = 100):
     print("trying")
+
     videos = scrapetube.get_search(search_query, limit=limit)
     video_id = []
     for video in videos:
+        # print(video)
         video_id.append(video["videoId"])
     if len(video_id) == 0:
         get_random_link(search_query)
@@ -31,23 +34,22 @@ def get_search_query():
 
     return search_query
 
-def write_json(new_data, filename='data.json'):
-    with open(filename,'r+') as file:
+
+def write_json(new_data, filename="data.json"):
+    with open(filename, "r+") as file:
         file_data = json.load(file)
-        file_data["ids"].update(new_data)
+        file_data["id"].update(new_data)
         file.seek(0)
-        json.dump(file_data, file, indent = 4)
- 
+        json.dump(file_data, file, indent=4)
+
 
 def main():
     search_query = get_search_query()
     link = get_random_link(search_query)
-    linkjson = {str(date.today()):link}
-    write_json(linkjson,"ytrandlinks.json")
+    linkjson = {"videoID": link, "generatedAt": str(date.today())}
+    write_json(linkjson, "ytrandlinks.json")
     print(link)
 
 
 if __name__ == "__main__":
     main()
-
-
